083a4e9519c1501b5c28c14b8564d29b44cbf598
hermit4 <hermit4loop@gmail.com>
2025-06-25 22:55:53 +0900

    ZZuse interrupt

--- waveshare/lcd-1.69-touch-investigation/src/main.rs
+++ waveshare/lcd-1.69-touch-investigation/src/main.rs
@@ -1,23 +1,31 @@
 #![no_std]
 #![no_main]
 
+use crate::pac::interrupt;
 use bsp::entry;
 use bsp::hal::{
     clocks::{init_clocks_and_plls, Clock},
     fugit::RateExtU32,
+    gpio::{self, Interrupt as GpioInterrupt},
     pac,
     sio::Sio,
     uart::{DataBits, StopBits, UartConfig},
     watchdog::Watchdog,
     I2C,
 };
+use core::cell::RefCell;
+use core::sync::atomic::{AtomicBool, Ordering};
+use cortex_m::interrupt::Mutex;
 use defmt::*;
 use defmt_rtt as _;
-use embedded_hal::digital::InputPin;
-use embedded_hal::digital::OutputPin;
+use embedded_hal::digital::{InputPin, OutputPin};
 use embedded_hal::i2c::I2c;
 use panic_probe as _;
 use rp_pico as bsp;
+
+type IrqPin = gpio::Pin<gpio::bank0::Gpio21, gpio::FunctionSio<gpio::SioInput>, gpio::PullUp>;
+static IRQ_PIN: Mutex<RefCell<Option<IrqPin>>> = Mutex::new(RefCell::new(None));
+static TOUCH_IRQ_PENDING: AtomicBool = AtomicBool::new(false);
 
 #[entry]
 fn main() -> ! {
@@ -66,7 +74,11 @@ fn main() -> ! {
     let mut rst = pins.gpio22.into_push_pull_output();
     rst.set_high().unwrap();
 
-    let mut irq = pins.gpio21.into_pull_up_input();
+    let irq = pins.gpio21.into_pull_up_input();
+    irq.set_interrupt_enabled(GpioInterrupt::LevelLow, true);
+    cortex_m::interrupt::free(|cs| {
+        IRQ_PIN.borrow(cs).replace(Some(irq));
+    });
 
     let uart_pins = (pins.gpio0.into_function(), pins.gpio1.into_function());
     let uart = bsp::hal::uart::UartPeripheral::new(pac.UART0, uart_pins, &mut pac.RESETS)
@@ -92,16 +104,42 @@ fn main() -> ! {
             defmt::warn!("I2C error");
         }
     }
+    unsafe {
+        pac::NVIC::unmask(pac::Interrupt::IO_IRQ_BANK0);
+    }
 
     loop {
-        if irq.is_low().unwrap() {
-            let mut touch_data = [0u8; 6];
-            if i2c.write_read(0x15u8, &[0x01], &mut touch_data).is_ok() {
-                let gesture = touch_data[0];
-                let x = ((touch_data[2] as u16 & 0x0F) << 8) | touch_data[3] as u16;
-                let y = ((touch_data[4] as u16 & 0x0F) << 8) | touch_data[5] as u16;
-                defmt::info!("Gesture: {}, X: {}, Y: {}", gesture, x, y);
+        cortex_m::interrupt::free(|cs| {
+            if TOUCH_IRQ_PENDING.load(Ordering::SeqCst) {
+                TOUCH_IRQ_PENDING.store(false, Ordering::SeqCst);
+                let mut touch_data = [0u8; 6];
+                if i2c.write_read(0x15u8, &[0x01], &mut touch_data).is_ok() {
+                    let gesture = touch_data[0];
+                    let x = ((touch_data[2] as u16 & 0x0F) << 8) | touch_data[3] as u16;
+                    let y = ((touch_data[4] as u16 & 0x0F) << 8) | touch_data[5] as u16;
+                    defmt::info!("Gesture: {}, X: {}, Y: {}", gesture, x, y);
+                }
+                IRQ_PIN
+                    .borrow(cs)
+                    .borrow()
+                    .as_ref()
+                    .unwrap()
+                    .set_interrupt_enabled(GpioInterrupt::LevelLow, true);
             }
-        }
+        });
+        cortex_m::asm::wfe();
     }
+}
+
+#[interrupt]
+fn IO_IRQ_BANK0() {
+    cortex_m::interrupt::free(|cs| {
+        let mut pin = IRQ_PIN.borrow(cs).borrow_mut();
+        let pin = pin.as_mut().unwrap();
+        if pin.is_low().unwrap() {
+            TOUCH_IRQ_PENDING.store(true, Ordering::SeqCst);
+        }
+        pin.set_interrupt_enabled(GpioInterrupt::LevelLow, false);
+        pin.clear_interrupt(GpioInterrupt::LevelLow);
+    });
 }
